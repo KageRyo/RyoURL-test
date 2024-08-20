784917b601f5ccde6f316e17e343ce465c54d2d5
影凌 <k6.developer@gmail.com>
2024-08-20 16:06:46 +0800

    modify: locustfile

--- stress_tests/locustfile.py
+++ stress_tests/locustfile.py
@@ -1,5 +1,7 @@
 import os
+import sys
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 BASE_URL = os.getenv('BASE_URL', 'http://172.21.0.2:8000/api')
 
 from anonymous_user import AnonymousUser
