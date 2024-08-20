31f38d396402e56ca8c308450568966997d58686
影凌 <k6.developer@gmail.com>
2024-08-20 16:56:05 +0800

    test: update short_url_apis unit test

--- unit_tests/test_short_url_apis.py
+++ unit_tests/test_short_url_apis.py
@@ -7,14 +7,14 @@ from schemas.schemas import UrlSchema
 """
 
 # GET /short-url/origin/{short_string} 取得原始網址
-def test_anonymous_can_get_original_url(api_client):
+def test_anonymous_can_get_original_url(anonymous_actions):
     # 首先創建一個短網址
     data = {"origin_url": "https://www.example.com"}
-    create_response = api_client.post("short-url/short", json=data)
+    create_response = anonymous_actions.create_short_url(data["origin_url"])
     short_string = UrlSchema(**create_response.json()).short_string
 
     # 然後獲取原始 URL
-    response = api_client.get(f"short-url/origin/{short_string}")
+    response = anonymous_actions.get_original_url(short_string)
     assert response.status_code == HTTPStatus.OK
     
     url_data = UrlSchema(**response.json())
@@ -22,9 +22,9 @@ def test_anonymous_can_get_original_url(api_client):
     assert url_data.creator_username == "anonymous"
 
 # POST /short-url/short 創建短網址 (匿名用戶)
-def test_anonymous_can_create_short_url(api_client):
+def test_anonymous_can_create_short_url(anonymous_actions):
     data = {"origin_url": "https://www.example.com"}
-    response = api_client.post("short-url/short", json=data)
+    response = anonymous_actions.create_short_url(data["origin_url"])
     assert response.status_code == HTTPStatus.CREATED
     
     # 使用 UrlSchema 驗證回應
@@ -33,9 +33,9 @@ def test_anonymous_can_create_short_url(api_client):
     assert url_data.creator_username == "anonymous"
 
 # POST /short-url/short 創建短網址 (已登入用戶)
-def test_user_can_create_short_url(user_client):
+def test_user_can_create_short_url(user_actions):
     data = {"origin_url": "https://www.example.com"}
-    response = user_client.post("short-url/short", json=data)
+    response = user_actions.create_short_url(data["origin_url"])
     assert response.status_code == HTTPStatus.CREATED
     
     url_data = UrlSchema(**response.json())
