6a0b5ae009ac6b6cde33c87d7bd9d64bf3da26bd
影凌 <k6.developer@gmail.com>
2024-08-20 16:40:40 +0800

    feat: add anonymous_actions

---
+++ actions/anonymous_actions.py
@@ -0,0 +1,27 @@
+class AnonymousActions:
+    def __init__(self, client):
+        self.client = client
+
+    def create_short_url(self, origin_url):
+        return self.client.post("short-url/short", json={"origin_url": origin_url})
+
+    def get_original_url(self, short_string):
+        return self.client.get(f"short-url/origin/{short_string}")
+
+    def register_user(self, username, password):
+        return self.client.post("auth/register", json={
+            "username": username,
+            "password": password
+        })
+        
+    def get(self, path):
+        return self.client.get(path)
+    
+    def post(self, path, json=None):
+        return self.client.post(path, json=json)
+
+    def put(self, path, params=None):
+        return self.client.put(path, params=params)
+
+    def delete(self, path):
+        return self.client.delete(path)
